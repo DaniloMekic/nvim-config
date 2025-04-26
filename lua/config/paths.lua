0df69952c65406d3128cacbd99fd9a1e38e88d46
DaniloMekic <danilo.mekic@gmail.com>
2025-04-26 23:50:12 +0200

    Update path for java-debug.

--- lua/config/paths.lua
+++ lua/config/paths.lua
@@ -14,7 +14,7 @@ else
   M.jdk11_home = vim.fn.glob("$ProgramFiles\\Amazon Corretto\\jdk11*")
   M.jdk17_home = vim.fn.glob("$ProgramFiles\\Amazon Corretto\\jdk17*")
   M.jdk21_home = vim.fn.glob("$ProgramFiles\\Amazon Corretto\\jdk21*")
-  local jdtls_java_debug_server_home = vim.fn.glob("~\\Programs\\java-debug-*")
+  local jdtls_java_debug_server_home = vim.fn.glob("~$LocalAppData\\Programs\\java-debug-*")
   M.jdtls_java_debug_server_plugin = jdtls_java_debug_server_home .. "\\com.microsoft.java.debug.plugin\\target\\com.microsoft.java.debug.plugin-0.53.1.jar"
 end
 
