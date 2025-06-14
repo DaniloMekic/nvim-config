7df312d43a9558d6b62865419d59d0b8f9e96917
DaniloMekic <danilo.mekic@gmail.com>
2025-06-14 23:14:12 +0200

    Change colorscheme to Kanagawa.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,37 @@
 -- Kanagawa
--- return { "rebelot/kanagawa.nvim" }
-
--- Catppuccin
-return { 
-  "catppuccin/nvim",
+-- https://github.com/rebelot/kanagawa.nvim
+return {
+  "rebelot/kanagawa.nvim",
   lazy = false,
-  name = "catppuccin",
   priority = 1000,
+  opts = {
+    theme = "wave"
+  },
   config = function()
-    vim.cmd([[colorscheme catppuccin]])
+    vim.cmd([[colorscheme kanagawa]])
   end
 }
+
+-- Catppuccin
+-- https://github.com/catppuccin/nvim
+-- return {
+--   "catppuccin/nvim",
+--   lazy = false,
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd([[colorscheme catppuccin]])
+--   end
+-- }
+
+-- Tokyo Night
+-- https://github.com/folke/tokyonight.nvim
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function ()
+--     vim.cmd[[colorscheme tokyonight]]
+--   end
+-- }
