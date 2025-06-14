967c9bacc9b554504357e0ac6de7be1f2a9a3138
DaniloMekic <danilo.mekic@gmail.com>
2025-06-14 07:28:52 +0200

    Add mini.icons and nvim-web-devicons.

---
+++ lua/plugins/icons.lua
@@ -0,0 +1,14 @@
+return {
+  -- https://github.com/echasnovski/mini.icons
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    opts = {}
+  },
+
+  -- https://github.com/nvim-tree/nvim-web-devicons
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {}
+  }
+}
