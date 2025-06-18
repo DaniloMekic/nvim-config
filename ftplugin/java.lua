3bf27538a07f58008e9d4565d9935a5b9587937c
DaniloMekic <danilo.mekic@gmail.com>
2025-06-18 08:29:10 +0200

    Add Java runtimes to JDTLS settings.

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -65,6 +65,30 @@ local config = {
   -- for a list of options
   settings = {
     java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "JavaSE-1.8",
+            path = "/usr/lib/jvm/java-8-openjdk"
+          },
+          {
+            name = "JavaSE-11",
+            path = "/usr/lib/jvm/java-11-openjdk"
+          },
+          {
+            name = "JavaSE-17",
+            path = "/usr/lib/jvm/java-17-openjdk"
+          },
+          {
+            name = "JavaSE-21",
+            path = "/usr/lib/jvm/java-21-openjdk"
+          },
+          {
+            name = "JavaSE-24",
+            path = "/usr/lib/jvm/java-24-openjdk"
+          }
+        }
+      }
     }
   },
 
