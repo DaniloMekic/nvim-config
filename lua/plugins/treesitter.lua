916b4734d74727a58a34993bfbf9be2511fef48a
DaniloMekic <danilo.mekic@gmail.com>
2025-09-02 09:49:58 +0200

    Apply formatting to Tree-sitter config file.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,59 +1,59 @@
 return {{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = {
-            -- Languages
-            "c",
-            "java",
-            "python",
-            "lua",
-            "javascript",
-            "vim", -- Vimscript
-            "sql",
-            "bash",
-            "awk",
-            -- Markup
-            "markdown",
-            "markdown_inline",
-            "html",
-            "css",
-            "json",
-            "yaml",
-            "toml",
-            "csv", -- CSV, PSV, TSV
-            -- Configs & Configuration Languages
-            "git_config", "gitattributes", "gitcommit", "gitignore",
-            "ssh_config",
-            "tmux",
-            "passwd",
-            "hyprlang", -- Hypr Configuration Language
-            "properties", -- Java Properties File
-            -- Docs
-            "vimdoc",
-            "luadoc",
-            "query"
-          },
-          sync_install = false,
-          highlight = {
-            enable = true
-          },
-          indent = {
-            enable = true -- See :help indent-expression and :help 'indentexpr'
-          },
-          incremental_selection = {
-            enable = true,
-            keymaps = {
-              -- Set to `false` to disable one of the mappings
-              init_selection = "gnn",
-              node_incremental = "grn",
-              scope_incremental = "grc",
-              node_decremental = "grm",
-            },
-          },
-        })
-    end
+    configs.setup({
+      ensure_installed = {
+        -- Languages
+        "c",
+        "java",
+        "python",
+        "lua",
+        "javascript",
+        "vim", -- Vimscript
+        "sql",
+        "bash",
+        "awk",
+        -- Markup
+        "markdown",
+        "markdown_inline",
+        "html",
+        "css",
+        "json",
+        "yaml",
+        "toml",
+        "csv", -- CSV, PSV, TSV
+        -- Configs & Configuration Languages
+        "git_config", "gitattributes", "gitcommit", "gitignore",
+        "ssh_config",
+        "tmux",
+        "passwd",
+        "hyprlang", -- Hypr Configuration Language
+        "properties", -- Java Properties File
+        -- Docs
+        "vimdoc",
+        "luadoc",
+        "query"
+      },
+      sync_install = false,
+      highlight = {
+        enable = true
+      },
+      indent = {
+        enable = true -- See :help indent-expression and :help 'indentexpr'
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          -- Set to `false` to disable one of the mappings
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+    })
+  end
 }}
