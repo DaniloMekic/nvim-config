6491679c6549d79f86974cb5b131dc8d5b1368d3
DaniloMekic <danilo.mekic@gmail.com>
2025-09-02 08:28:25 +0200

    Remove tree-sitter grammar and parser for Elixir and HEEx.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,10 @@ return {{
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "java" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "java" },
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
         })
     end
 }}
