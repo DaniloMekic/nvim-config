8a75f66281659d8153e645dc8778e2aefc02884b
DaniloMekic <danilo.mekic@gmail.com>
2025-09-02 09:13:47 +0200

    Expand Tree-sitter configuration with more modules.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,16 +6,36 @@ return {{
 
       configs.setup({
           ensure_installed = {
+            -- Languages
             "c",
+            "java",
+            "python",
             "lua",
-            "vim",
-            "vimdoc",
-            "query",
             "javascript",
+            "vim", -- Vimscript
+            "sql",
+            "bash",
+            "awk",
+            -- Markup
+            "markdown",
+            "markdown_inline",
             "html",
             "css",
-            "java",
-            "python"
+            "json",
+            "yaml",
+            "toml",
+            "csv", -- CSV, PSV, TSV
+            -- Configs & Configuration Languages
+            "git_config", "gitattributes", "gitcommit", "gitignore",
+            "ssh_config",
+            "tmux",
+            "passwd",
+            "hyprlang", -- Hypr Configuration Language
+            "properties", -- Java Properties File
+            -- Docs
+            "vimdoc",
+            "luadoc",
+            "query"
           },
           sync_install = false,
           highlight = { enable = true },
