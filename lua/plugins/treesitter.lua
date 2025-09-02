e73cdad2cdc35752f1340cf24fb6c5992cc71d59
DaniloMekic <danilo.mekic@gmail.com>
2025-09-02 08:31:45 +0200

    Apply better formatting to the tree-sitter config.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,18 @@ return {{
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "java" },
+          ensure_installed = {
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "javascript",
+            "html",
+            "css",
+            "java",
+            "python"
+          },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
