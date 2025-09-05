f06afabc6c8e59246a8fc0b826b2bf8a4aad2b5d
DaniloMekic <danilo.mekic@gmail.com>
2025-09-05 11:44:55 +0200

    Small code formatting change for LSP config.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(e)
-    local bufopts = {buffer = e.buf }
+    local bufopts = { buffer = e.buf }
     -- See :help lsp-defaults
     vim.keymap.set("n", "grd", vim.lsp.buf.definition, bufopts)
   end
