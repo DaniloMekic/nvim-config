15c844851d32f7a7de7ebaf9ee404e34c9cb9e4d
DaniloMekic <danilo.mekic@gmail.com>
2025-09-17 22:38:29 +0200

    Enable signature help in Blink.cmp.

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -34,6 +34,8 @@ return {
     -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
 
+    signature = { enabled = true },
+
     snippets  = { preset = "luasnip" },
 
     -- Default list of enabled providers defined so that you can extend it
