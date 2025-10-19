63ff07f0c90837874e37583f6843d5c3bf09c956
DaniloMekic <danilo.mekic@gmail.com>
2025-10-19 22:24:26 +0200

    Update keybind for quickfix window to be toggle, instead of relying on separate binds for opening/closing.

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -7,6 +7,9 @@
 
 vim.g.mapLeader = " "
 
+vim.keymap.set("n", "<Leader>w", ":write<CR>")
+vim.keymap.set("n", "<Leader>q", ":quit<CR>")
+
 -- Buffers Navigation
 -- See `:help CTRL-^` for switching to the previously edited file.
 vim.keymap.set("n", "<Leader><Tab>", ":bnext<CR>")                                                 -- Switch window to the next buffer.
@@ -14,8 +17,24 @@ vim.keymap.set("n", "<Leader><S-Tab>", ":bprevious<CR>")                        
 
 -- QuickFix List
 -- See `:help default-mappings`; `:help [q`; `:help ]q`
-vim.keymap.set("n", "<Leader>q", ":copen<CR>")                                                     -- Open quickfix window
-vim.keymap.set("n", "<Leader>Q", ":cclose<CR>")                                                    -- Close quickfix window
+vim.keymap.set(
+    "n",
+    "<Leader>q",
+    function ()
+        -- Get list of all windows
+        local windows = vim.api.nvim_list_wins()
+        -- Check if any is a quickfix window
+        for _, window in pairs(windows) do
+            local buffer = vim.api.nvim_win_get_buf(window)
+            if vim.bo[buffer].buftype == "quickfix" then
+                vim.cmd("cclose")
+                return
+            end
+        end
+        -- If none found, open quickfix
+        vim.cmd("copen")
+    end,
+    { desc = "Toggle quickfix window" })
 
 -- Oil
 local oil = require("oil")
