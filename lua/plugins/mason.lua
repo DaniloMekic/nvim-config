74ac611c2d277aff31927da6001dd4671c217351
DaniloMekic <danilo.mekic@gmail.com>
2025-11-09 20:57:51 +0100

    Add LSP for LaTeX.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,21 +4,22 @@ return {
         ---@type string[]
         ensure_installed = {
             -- Valid LSP Names: github.com/neovim/nvim-lspconfig/tree/master/lsp
-            "vimls",                 -- VimScript
-            "jdtls",                 -- Java
-            "clangd",                -- C, C++
-            "lua_ls",                -- Lua
-            "html",                  -- HTML
-            "cssls",                 -- CSS
-            "ts_ls",                 -- TypeScript
-            "basedpyright",          -- Python LSP
-            "ruff",                  -- Python Linter/Code Formatter
-            "bashls",                -- Bash
-            "markdown_oxide",        -- Markdown
-            "lemminx",               -- XML
-            "yamlls",                -- YAML
-            "taplo",                 -- TOML
-            "docker_language_server" -- Docker
+            "vimls",                   -- VimScript
+            "jdtls",                   -- Java
+            "clangd",                  -- C, C++
+            "lua_ls",                  -- Lua
+            "html",                    -- HTML
+            "cssls",                   -- CSS
+            "ts_ls",                   -- TypeScript
+            "basedpyright",            -- Python LSP
+            "ruff",                    -- Python Linter/Code Formatter
+            "bashls",                  -- Bash
+            "markdown_oxide",          -- Markdown
+            "lemminx",                 -- XML
+            "yamlls",                  -- YAML
+            "taplo",                   -- TOML
+            "docker_language_server",  -- Docker
+            "texlab",                  -- LaTeX
         },
         ---@type boolean | string[] | { exclude: string[] }
         automatic_enable = {
