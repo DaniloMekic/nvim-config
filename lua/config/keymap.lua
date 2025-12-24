de4abf6976979f60cffaf8e1527b4de20b193de6
DaniloMekic <danilo.mekic@gmail.com>
2025-12-24 16:18:37 +0100

    Simplify function for keymap that opens quickfix window.

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -15,17 +15,10 @@ vim.keymap.set(
     "n",
     "<Leader>q",
     function ()
-        -- Get list of all windows
-        local windows = vim.api.nvim_list_wins()
-        -- Check if any is a quickfix window
-        for _, window in pairs(windows) do
-            local buffer = vim.api.nvim_win_get_buf(window)
-            if vim.bo[buffer].buftype == "quickfix" then
-                vim.cmd("cclose")
-                return
-            end
+        if vim.fn.getqflist({ winid = 0 }).winid ~= 0 then
+            vim.cmd("cclose")
+        else
+            vim.cmd("copen")
         end
-        -- If none found, open quickfix
-        vim.cmd("copen")
     end,
     { desc = "Toggle quickfix window" })
